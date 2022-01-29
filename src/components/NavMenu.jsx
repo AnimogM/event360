ed7eeed9ce0340389e493709c9584f972bd00c54
gomina maryam <gominamaryam@gmail.com>
2022-01-29 02:16:03 +0100

    initial setup

---
+++ src/components/NavMenu.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Row,Col} from 'react-bootstrap'
+
+import Logo from '../images/logo360'
+
+const NavMenu = () => {
+  return <>
+  <Row>
+      
+  </Row>
+  </>;
+};
+
+export default NavMenu;
---
+++ src/pages/CreateEvent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CreateEvent = () => {
+  return <div>new</div>;
+};
+
+export default CreateEvent;
